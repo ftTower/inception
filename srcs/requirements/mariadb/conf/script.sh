22e7384e7c7b7cac1defbea2f757f92f76ffe1f5
ftTower <thomas.auer67@gmail.com>
2025-01-21 15:31:25 +0100

    update Dockerfiles to use Debian Bullseye; add PHP-FPM configuration for WordPress; enhance MariaDB initialization script; modify Nginx config for improved logging and PHP handling

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-service mysql start;
+chown mysql -R /var/lib/mysql
+mkdir -p /var/run/mysqld/
+chown mysql -R /var/run/mysqld
+mysql_upgrade --user=mysql --datadir=/var/lib/mysql
+
+service mariadb start;
+
+mysqladmin status --wait
 
 # Configurer la base de données et les utilisateurs
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -e "FLUSH PRIVILEGES;"
 
 # Attendre la fin du processus MySQL si nécessaire
 # wait $MYSQL_PID
 
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
+service mariadb stop;
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
+set -x
 
 until mysqladmin ping -h mariadb --silent; do
     echo "Waiting for database..."
     sleep 2
 done
 
-wp config create --allow-root \
-    --dbname=$SQL_DATABASE \
-    --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD \
-    --dbhost=mariadb:3306 \
-    --path='/var/www/html'
 
-exec php-fpm7.3 --nodaemonize
+if ! [ -e "/var/www/html/wp-config.php" ] ; then
+    wp core download --path=/var/www/html --locale=en_US --allow-root
+
+    wp config create --allow-root \
+        --dbname=$SQL_DATABASE \
+        --dbuser=$SQL_USER \
+        --dbpass=$SQL_PASSWORD \
+        --dbhost=mariadb:3306 \
+        --path='/var/www/html'
+
+    wp core install \
+        --url="tauer.42.fr" \
+        --title="OMELETTE DE FROMAGE" \
+        --admin_user="$WP_AUSER" \
+        --admin_password="$WP_APASS" \
+        --admin_email="$WP_AMAIL" \
+        --path='/var/www/html' \
+        --allow-root
+	wp user create "$WP_USER" "$WP_MAIL" \
+        --user_pass="$WP_PASS" \
+        --path='/var/www/html' \
+        --allow-root
+fi
+mkdir -p /run/php/
+exec php-fpm7.4 --nodaemonize
